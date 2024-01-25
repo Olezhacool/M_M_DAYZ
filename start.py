d33a7098577c2d43e00178031c167835ee9c749d
Иван <umnovasvetlana5@gmail.com>
2024-01-25 21:31:25 +0300

    НЕ СМОТРЕТЬ

--- start.py
+++ start.py
@@ -6,6 +6,9 @@ import os
 
 FPS = 60
 pygame.init()
+hs = []
+ds = []
+rs = []
 clock = pygame.time.Clock()
 
 size = width, height = 900, 750
@@ -97,7 +100,7 @@ class Door1(pygame.sprite.Sprite):
         super().__init__(all_sprites, door_group)
         self.frames = []
         self.open = 0
-        self.sheet = load_image("lil house.png")
+        self.sheet = load_image("lil house d.png")
         self.cut_sheet(self.sheet, 1, 1)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
@@ -115,53 +118,9 @@ class Door1(pygame.sprite.Sprite):
                     frame_location, self.rect.size)))
 
 
-class House(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(all_sprites, house_group)
-        self.image1 = load_image("lil house inside.png")
-        self.image2 = load_image("lil house.png")
-        self.sheet = load_image("lil house.png")
-        self.frames = []
-        self.sheett(self.sheet)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.image.get_rect()
-        self.rect = self.image2.get_rect()
-        self.rect.x, self.rect.y = tile_width * pos_x, tile_height * pos_y
-
-    def sheett(self, sheet):
-        self.rect = pygame.Rect(0, 0, sheet.get_width(),
-                                sheet.get_height())
-        frame_location = (self.rect.w, self.rect.h)
-        self.frames.append(sheet.subsurface(pygame.Rect(
-            frame_location, self.rect.size)))
-
-
 def terminate():
     pygame.quit()
     sys.exit()
-
-
-def load_level(file):
-    file = f'data/{file}'
-    with open(file, 'r') as f:
-        map_level = list(map(str.strip, f.readlines()))
-    max_width = 110
-    step = 10
-    towns = 9
-    level = list(map(lambda x: x.ljust(max_width - 15, '0'), map_level))
-    level = list(map(lambda x: x.rjust(max_width, '1'), level))
-    level[3] = level[3][:7] + '@' + level[3][8:]
-    x = sample(range(15, 110, step), towns)
-    y = sample(range(0, 90, step), towns)
-    for i in y:
-        for j in range(i, i + 10):
-            print(j)
-            do = level[j][:x[y.index(i)]]
-            posle = level[j][x[y.index(i)] + 10:]
-            level[j] = do + "2222222222" + posle
-    print(level)
-    return level
 
 
 class Tile(pygame.sprite.Sprite):
@@ -175,26 +134,29 @@ class Tile(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(all_sprites, player_group)
-        self.x = 270
-        self.y = 320
+        self.x = 0
+        self.y = 0
         self.move = True
         self.rifle = Nothing()
+        self.rows = 0
+        self.f_l = []
         self.frames = []
         self.sheet = load_image("gg.png")
-        self.cut_sheet(self.sheet, 12, 1)
+        self.cut_sheet(12, 1)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.mask = pygame.mask.from_surface(load_image("ggm.png"))
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = tile_width * pos_x, tile_height * pos_y
+        self.rect.x, self.rect.y = 56, 56
 
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
+    def cut_sheet(self, columns, rows):
+        self.rect = pygame.Rect(415, 340, self.sheet.get_width() // columns,
+                                self.sheet.get_height() // (rows))
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
+                self.f_l.append(frame_location)
+                self.frames.append(self.sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
     def update(self):
@@ -215,17 +177,29 @@ class Player(pygame.sprite.Sprite):
             else:
                 hs[ds.index(d)].image = load_image('lil house comb.png')
                 hs[ds.index(d)].mask = pygame.mask.from_surface(load_image('lil house comb.png'))
+        for r in rs:
+            if pygame.sprite.collide_mask(self, r):
+                r.rect.x, r.rect.y = player.rect.x + 2, player.rect.y - 7
+                self.rifle = r
+                self.frames = []
+                self.sheet = load_image("gg with ak.png")
+                self.cut_sheet(12, 1)
+
+
+
 
 
 class Rifle(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, n=0):
+    def __init__(self, sheet, pos_x, pos_y, n=0):
         super().__init__(all_sprites, gun_group)
         self.frames = []
         self.sheet = sheet
-        self.cut_sheet(self.sheet, columns, rows)
+        self.cut_sheet(self.sheet, 4, 1)
+        self.mask = pygame.mask.from_surface(load_image("ak.png"))
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
-        self.rect.x, self.rect.y = player.rect.x - 10 + n, player.rect.y - 4
+        self.rect.x, self.rect.y = tile_width * pos_x, tile_height * pos_y
+        # self.rect.x, self.rect.y = player.rect.x - 10 + n, player.rect.y - 4
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -242,8 +216,9 @@ class Rifle(pygame.sprite.Sprite):
 
 
 class AK(Rifle):
-    def __init__(self, sheet, columns, rows, n=0):
-        super().__init__(load_image("AK-47l.png"), columns, rows, n=0)
+    def __init__(self, pos_x, pos_y, n=0):
+        super().__init__(load_image("kalash.png"), pos_x, pos_y, n=0)
+        self.withrf = load_image("gg with ak.png")
 
 
 class Nothing:
@@ -261,6 +236,9 @@ def generate_level(level):
                 Tile('sand', x, y)
             elif level[y][x] == '2':
                 Tile('stone', x, y)
+            # elif level[y][x] == '3':
+            #     hs.append(House1(x, y))
+            #     ds.append(Door1(x, y))
             elif level[y][x] == '@':
                 Tile('sand', x, y)
                 np = Player(x, y)
@@ -289,18 +267,15 @@ tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 gun_group = pygame.sprite.Group()
 house_group = pygame.sprite.Group()
-rifle = Nothing()
 running = True
 camera = Camera()
 player, level_x, level_y = generate_level(load_level('level1.txt'))
-print("\n".join(load_level('level1.txt')))
-hs = []
-ds = []
-for i in range(3):
-    x = randint(0, 50)
-    y = randint(0, 50)
-    hs.append(House1(x, y))
-    ds.append(Door1(x, y))
+# for i in range(3):
+#     x = randint(0, 50)
+#     y = randint(0, 50)
+#     hs.append(House1(x, y))
+#     ds.append(Door1(x, y))
+rs.append(AK(0, 0, 0))
 STEP = 4
 e = 0
 to_move = False
@@ -309,7 +284,7 @@ to_right = False
 to_up = False
 to_down = False
 to_shoot = False
-coord = [0]
+coord = [0, 0]
 kx = 0
 ky = 0
 yy = 0
@@ -322,24 +297,23 @@ while running:
         if event.type == pygame.MOUSEMOTION:
             coord = event.pos
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_s:
                 to_shoot = True
             if event.key == pygame.K_d:
                 to_move = True
         # если пользователь перестал нажимать клавишу
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_s:
                 to_shoot = False
             if event.key == pygame.K_d:
                 to_move = False
     # В зависимости от значений переменных направления
     # меняем значения координат
-    if to_shoot and rifle.__class__.__name__ != 'Nothing':
-        rifle.cur_frame = (rifle.cur_frame + 1) % len(rifle.frames)
+    if to_shoot and player.rifle.__class__.__name__ != 'Nothing':
+        player.rifle.cur_frame = (player.rifle.cur_frame + 1) % len(player.rifle.frames)
     if to_right or to_left or to_down or to_up:
         player.cur_frame = (player.cur_frame + 1) % len(player.frames)
     if to_move and player.move:
-        # доработать движение, только наискосок пока ходит
         player.cur_frame = (player.cur_frame + 1) % len(player.frames)
         xy = abs(coord[0] - width // 2) + abs(coord[1] - height // 2)
         xx = abs(coord[0] - width // 2) / (xy + 0.000001)
@@ -354,6 +328,8 @@ while running:
             ky = 1
         player.rect.x += STEP * xx * kx
         player.rect.y += STEP * yy * ky
+        player.x += STEP * xx * kx
+        player.y += STEP * yy * ky
     elif not player.move:
         xy = abs(coord[0] - width // 2) + abs(coord[1] - height // 2)
         xx = abs(coord[0] - width // 2) / xy
@@ -370,6 +346,8 @@ while running:
             player.move = True
             player.rect.x -= STEP * xx * kx
             player.rect.y -= STEP * yy * ky
+            player.x -= STEP * xx * kx
+            player.y -= STEP * yy * ky
 
     camera.update(player)
     for sprite in all_sprites:
@@ -382,4 +360,4 @@ while running:
     all_sprites.update()
     gun_group.draw(screen)
     clock.tick(30)
-    pygame.display.flip()
+    pygame.display.flip() 
